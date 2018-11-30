3e0965fba48295c0c9a7168ccfb0ceb8f3e5f6ae
Martin Jensen <ingvar@netaben.dk>
2018-11-30 15:31:25 +0100

    Cleaning the code

--- src/main/java/bn/supermod/rainbow/blocks/RainbowDirtBlock.java
+++ src/main/java/bn/supermod/rainbow/blocks/RainbowDirtBlock.java
@@ -34,67 +34,9 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-/*
- * 
- * 255,   0,   0
-255, 128,   0
-255, 255,   0
-128, 255,   0
-  0, 255,   0
-  0, 255, 128
-  0, 255, 255
-  0, 128, 255
-  0,   0, 255
-128,   0, 255
-255,   0, 255
-255,   0, 128
-
-
-255,   0,   0
-255,  64,   0
-255, 128,   0
-255, 192,   0
-255, 255,   0
-192, 255,   0
-128, 255,   0
- 64, 255,   0
-  0, 255,   0
-  0, 255,  64
-  0, 255, 128
-  0, 255, 192
-  0, 255, 255
-  0, 192, 255
-  0, 128, 255
-  0,  64, 255
-  0,   0, 255
- 64,   0, 255
-128,   0, 255
-192,   0, 255
-255,   0, 255
-255,   0, 192
-255,   0, 128
-255,   0, 64
- * 
- * */
-
 public class RainbowDirtBlock extends Block implements IHasModel {
 
-	/*public RainbowDirtBlock(String name, Material material, CreativeTabs tab) 
-	{
-		super(material);
-		setUnlocalizedName(name);
-		setRegistryName(name);
-		setCreativeTab(tab);
-		
-//		setHasSubtypes(true);
-		net.minecraft.block.BlockStainedGlass
-		
-		BlockInit.BLOCKS.add(this);
-		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(name));		
-	}*/
-	
 	public static final PropertyEnum<EnumRainbowColor> COLOR = PropertyEnum.<EnumRainbowColor>create("color", EnumRainbowColor.class);
-	//public static final PropertyEnum<EnumDyeColor> COLOR2 = PropertyEnum.<EnumDyeColor>create("variant", EnumDyeColor.class);
 
     public RainbowDirtBlock(String name, Material material, CreativeTabs tab)
     {
@@ -102,25 +44,10 @@ public class RainbowDirtBlock extends Block implements IHasModel {
         setUnlocalizedName(name);
 		setRegistryName(name);
 		setCreativeTab(tab);
-		
-        //this.setDefaultState(this.blockState.getBaseState().withProperty(COLOR, EnumDyeColor.WHITE));
-        this.setDefaultState(this.blockState.getBaseState().withProperty(COLOR, EnumRainbowColor.R4G2B0));
-              
-        
+        this.setDefaultState(this.blockState.getBaseState().withProperty(COLOR, EnumRainbowColor.R4G0B0));
         
         BlockInit.BLOCKS.add(this);
 		ItemInit.ITEMS.add(new RainbowDirtItemBlock(this).setRegistryName(name));
-				
-        //net.minecraft.block.BlockStainedGlass
-		//net.minecraft.block.BlockPlanks
-		//net.minecraft.block.BlockLog
-    }
-
-    @Override
-    public int damageDropped(IBlockState state)
-    {
-    	Main.logger.error("Meta (damageDropped): " + ((EnumRainbowColor)state.getValue(COLOR)).getMetadata());
-        return ((EnumRainbowColor)state.getValue(COLOR)).getMetadata();
     }
 
     @Override
@@ -128,81 +55,27 @@ public class RainbowDirtBlock extends Block implements IHasModel {
     {
         for (EnumRainbowColor color : EnumRainbowColor.values())
         {        	
-        	Main.logger.error("getSubBlocks: " + color.getMetadata());
             items.add(new ItemStack(this, 1, color.getMetadata()));
         }
     }
-    
-    
-    /*public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos)
-    {    	
-    	return MapColor.getBlockColor((EnumDyeColor)state.getValue(COLOR));
-    } */   
 
+    @Override
+    public int damageDropped(IBlockState state)
+    {
+        return ((EnumRainbowColor)state.getValue(COLOR)).getMetadata();
+    }
+    
     @Override
     public IBlockState getStateFromMeta(int meta)
     {
-    	Main.logger.error("getStateFromMeta: " + meta + " " + this.getDefaultState().withProperty(COLOR, EnumRainbowColor.byMetadata(meta)));
         return this.getDefaultState().withProperty(COLOR, EnumRainbowColor.byMetadata(meta));
     }
 
     @Override
     public int getMetaFromState(IBlockState state)
     {
-    	Main.logger.error("getMetaFromState: " + ((EnumRainbowColor)state.getValue(COLOR)).getMetadata() + " " + ((EnumRainbowColor)state.getValue(COLOR)).getMetadata());
         return ((EnumRainbowColor)state.getValue(COLOR)).getMetadata();
-    }
-
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
-    {    	
-    	Main.logger.error("getStateForPlacement: " + this.getStateFromMeta(meta) + " meta: " + meta);
-        return this.getStateFromMeta(meta);
-    }
-    
-    /*
-    @Override
-	public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-    	Main.logger.error("getActualState: " + state);
-		// TODO Auto-generated method stub
-		return super.getActualState(state, worldIn, pos);
-	}
-
-	@Override
-	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
-		// TODO Auto-generated method stub
-		Main.logger.error("onBlockAdded: " + state);
-		super.onBlockAdded(worldIn, pos, state);
-	}*/
-/*
-	@Override
-	public BlockStateContainer getBlockState() {
-		// TODO Auto-generated method stub
-		Main.logger.error("getBlockState: " + super.getBlockState());
-		return super.getBlockState();
-	}*/
-
-	
-	@Override
-	public TileEntity createTileEntity(World world, IBlockState state) {
-		// TODO Auto-generated method stub
-		Main.logger.error("createTileEntity: " + state);
-		return super.createTileEntity(world, state);
-	}
-
-	@Override
-	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer,
-			ItemStack stack) {
-		// TODO Auto-generated method stub
-    	Main.logger.error("onBlockPlacedBy: " + state);
-		super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
-	}
-
-	@Override
-	public boolean onBlockActivated(final World worldIn, final BlockPos pos, final IBlockState state, final EntityPlayer playerIn, final EnumHand hand, final EnumFacing facing, final float hitX, final float hitY, final float hitZ) 
-    {
-    	Main.logger.error("onBlockActivated: " + state);
-    	return worldIn.setBlockState(pos, state.cycleProperty(COLOR));
-	}
+    }        
     
     @Override
     protected BlockStateContainer createBlockState()
@@ -215,8 +88,7 @@ public class RainbowDirtBlock extends Block implements IHasModel {
 	{
 		for (EnumRainbowColor color : EnumRainbowColor.values())
         {
-			String registryName = this.getRegistryName() + "_" + color.getName();
-			Main.logger.info("Registering model for " + this.getRegistryName() + "_" + color.getName() + ", meta: " + color.getMetadata());
+			String registryName = this.getRegistryName() + "_" + color.getName();			
 			Main.proxy.registerModel(Item.getItemFromBlock(this), color.getMetadata(), registryName);
         }
 	}
